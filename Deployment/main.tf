47fb5a754addd0b9402b08b309e8b3b3da04c07b
gagasgit <95085194+gagasgit@users.noreply.github.com>
2022-06-06 10:53:01 +0400

    Update main.tf

--- Deployment/main.tf
+++ Deployment/main.tf
@@ -1,12 +1,3 @@
-#----------------------------------------------------------
-# Provision Highly Availabe Web in any Region Default VPC
-# Create:
-#    - Security Group for Web Server
-#    - Launch Configuration with Auto AMI Lookup
-#    - Auto Scaling Group using 2 Availability Zones
-#    - Classic Load Balancer in 2 Availability Zones
-#-----------------------------------------------------------
-
 provider "aws" {
   region = var.region
 }
