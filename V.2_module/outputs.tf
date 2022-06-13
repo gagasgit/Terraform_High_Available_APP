37dd8e86d91b93e1aec81558d867e58322fb84b9
gagasgit <95085194+gagasgit@users.noreply.github.com>
2022-06-13 10:17:03 +0400

    Delete V.2_module directory

--- V.2_module/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "latest_amazon_linux_ami_id" {
-  value = data.aws_ami.latest_amazon_linux.id
-}
-
-output "latest_amazon_linux_ami_name" {
-  value = data.aws_ami.latest_amazon_linux.name
-}
